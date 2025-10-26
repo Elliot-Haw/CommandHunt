d6424cca76a898fa494225c3fa15b1264602e95a
Elliot Haw <elliothaw@icloud.com>
2025-10-26 20:54:05 +1100

    Updated command hunt to urget the user to ask for help if they dont enter a command

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 # include <stdio.h>
 
 int main(int argc, char* argv[]){
-	printf("Hello World\n");
 	if (argc == 1){
-	printf("Zero Arguments");
+	printf("Please provide a command or see 'commandhunt --help'\n");
 	}
+	//else call Commands Function in here
 	return 0;
+	//Have a few cases that check if the arguments is greater than one and if it is run it in the Commands Header File
+	//
+	//Other wise print unknown command for a list of commands enter --h
+	//
+	//Notes - History can be found based on the shell the user is using
+	//If  the current session hasn't ended the session will still be live so the history won't be stored
 }
