cf253ebbc940cd1f11596f519412f3378e8715de
Elliot Haw <elliothaw@icloud.com>
2025-10-31 20:11:27 +1100

    adding comments to commands.c

--- main.c
+++ main.c
@@ -9,6 +9,4 @@ int main(int argc, char* argv[]){
 	checkCommands();
 	}
 	return 0;
-	//Notes - History can be found based on the shell the user is using
-	//If  the current session hasn't ended the session will still be live so the history won't be stored
 }
